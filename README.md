3ee87291a07be090490b5b79a3aa38006d72b6a5
febrihehe <febriandevander@gmail.com>
2023-04-09 17:23:21 +0700

    presesnispegawai

---
+++ README.md
@@ -0,0 +1,6 @@
+Nama Ketua:
+12211384_Febrian Devander
+
+Nama Anggota:
+12211366_Muhammad Nur Zaki Ahmad
+12211072_Howie Rio Julianto
---
+++ android/app/src/main/kotlin/id/ac/bsi/u4e_12211072/presensipegawai/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.ac.bsi.u4e_12211072.presensipegawai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
